433e2ab259d5650ed938cc42a3b61ac4de028884
JDMTias <matiasiturbide@gmail.com>
2020-05-28 16:36:13 -0600

    validation done

--- wmp/src/components/RegistrationForm.js
+++ wmp/src/components/RegistrationForm.js
@@ -1,10 +1,23 @@
 //This component is a form used for creating a new pair of login credentials for a user that does not have an account yet
 //form should have validation visible to user
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useHistory } from "react-router-dom";
+import formSchema from "../validation/formSchema";
+import * as yup from "yup";
+import {Link} from 'react-router-dom'
+
+const initialFormError = {
+  username: "",
+  password: "",
+};
+const initialDisabled = true;
 
 const RegistrationForm = (props) => {
+  const [formErrors, setFormErrors] = useState(initialFormError);
+  const [disabled, setDisabled] = useState(initialDisabled);
+  let history = useHistory();
   const [user, setUser] = useState({
     username: "",
     password: "",
@@ -16,31 +29,62 @@ const RegistrationForm = (props) => {
       ...user,
       [e.target.name]: e.target.value,
     });
+
+    const name = e.target.name;
+    const value = e.target.value;
+
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then((valid) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        });
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0],
+        });
+      });
   };
 
-    const submitForm = e => {
-        e.preventDefault();
-        const userCredentials = { username: user.username, password: user.password, phone_number: user.phone_number }
-        axiosWithAuth()
-            .post('/api/auth/register', userCredentials)
-            .then(res => {
-                console.log(res)
-            })
-            .catch( err => console.log(err) );
-        }
+  const submitForm = (e) => {
+    e.preventDefault();
+    const userCredentials = {
+      username: user.username,
+      password: user.password,
+      phone_number: user.phone_number,
+    };
+    axiosWithAuth()
+      .post("/api/auth/register", userCredentials)
+      .then((res) => {
+        console.log(res);
+        history.push("/home");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    formSchema.isValid(user).then((valid) => {
+      setDisabled(!valid);
+    });
+  }, [user]);
 
   return (
-    <div className = 'regForm'>
+    <div className="regForm">
       <h3>Welcome!</h3>
       <p>Please fill out all required fields</p>
-     <form onSubmit={submitForm}>
+      <form onSubmit={submitForm}>
         <input
           value={user.username}
           name="username"
           type="text"
-          placeholder="Username" 
-          onChange = {handleChanges}
+          placeholder="Username"
+          onChange={handleChanges}
         />
+        <div className="errors">{formErrors.username}</div>
         <input
           value={user.password}
           name="password"
@@ -48,16 +92,25 @@ const RegistrationForm = (props) => {
           placeholder="Password"
           onChange={handleChanges}
         />
+        <div className="errors">{formErrors.password}</div>
+
         <input
           value={user.phone_number}
           name="phone_number"
           type="text"
           placeholder="(999)999-9999"
-          onChange= {handleChanges}
+          onChange={handleChanges}
         />
-        <button type='submit'>Register</button>
+        <button type="submit" disabled={disabled}>
+          Register
+        </button>
+
+        <div>
+            <h3>Already a member?</h3>
+            <Link to= '/' className= 'singUpButton'>Login!</Link>
+        </div>
       </form>
-     </div>
+    </div>
   );
 };
 
